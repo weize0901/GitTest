b71661ad989130f6136a505fd9c6b5f6338f925e
weize0901 <449424392@qq.com>
2019-06-17 14:56:18 +0800

    add function test

--- GitTest/src/com/woniuxy/test/test.java
+++ GitTest/src/com/woniuxy/test/test.java
@@ -3,5 +3,10 @@ package com.woniuxy.test;
 public class test {
 public static void main(String[] args) {
 	System.out.println("hello git");
+	
+	
+}
+public void test(){
+	System.out.println("hi");
 }
 }
